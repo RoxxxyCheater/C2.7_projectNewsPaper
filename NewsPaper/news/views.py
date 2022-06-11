2bf59ea4f870f5c1f023a36e004c1657c1df5445
RoxxxyCheater <sayt@3g.ua>
2022-06-11 13:58:53 +0300

    Новая страничка с адресом /news/, на которой выводится список всех новостей.
    Все статьи выведены в виде заголовка, даты и первых 50 символов текста статьи.
    Новости выводятся в порядке от более свежей до самой старой. Отображается всё по адресу /news/.
    
    Сделана отдельная страница для полной информации о статье /news/<id новости>. На этой странице вся информация о статье. Название, текст и дата загрузки в формате ДЕНЬ-МЕСЯЦ-ГОД ЧАС:МИНУТЫ.
    
    Написан собственный фильтр Censor, который цензурирует нежелательную лексику в названиях и текстах статей.

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,13 +1,23 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
-from .models import Post
+from .models import Post,Author
+from datetime import datetime
  
  
 class PostList(ListView):
     model = Post  # указываем модель, объекты которой мы будем выводить
     template_name = 'news_all.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'news_all'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
- 
+    queryset = Post.objects.order_by('-id') # По такому запросу сформируется список объектов, которые будут выводиться в представлении/если не указывать по умолчанию сработает не .order_by('-id'), а .all()
+    #.order_by('-id') сортировка от нового к старому
+
+    # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словари и есть переменные, к которым мы сможем потом обратиться через шаблон
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow() # добавим переменную текущей даты time_now
+        context['value1'] = Author.objects.all().order_by('-rateAuthor').values('authors_id', 'rateAuthor')[0] # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
+        
 class PostDetail(DetailView): # адресс в котором будет лежать информация о конкретном товаре
     model = Post
     template_name ='news.html'
